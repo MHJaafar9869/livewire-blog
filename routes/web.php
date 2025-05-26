1eea341615923fa8d7c2048c2481d743babb4b67
MHJaafar9869 <mostafajaafar9869@gmail.com>
2025-05-26 09:38:40 +0300

    refactoring html

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn() => view('welcome'));
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])
-  ->group(function () {
-    Route::get('/dashboard', fn() => view('dashboard'))
-      ->name('dashboard');
-  });
+    ->group(function () {
+        Route::get('/dashboard', fn() => view('dashboard'))
+            ->name('dashboard');
+    });
