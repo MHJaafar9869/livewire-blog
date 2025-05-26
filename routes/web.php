8fb178e718a02b2f9230aededb99718292daf975
MHJaafar9869 <mostafajaafar9869@gmail.com>
2025-05-26 08:28:18 +0300

    Livewire blog project - following (Yelo Code)

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', fn() => view('welcome'));
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])
+  ->group(function () {
+    Route::get('/dashboard', fn() => view('dashboard'))
+      ->name('dashboard');
+  });
